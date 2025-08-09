f2bb093209b5f1c22ef791c53d6af5ae9374f966
Sonny Antwi Gyamerah <sonnyantwigyamerah@Sonnys-MacBook-Pro.local>
2025-08-09 11:08:20 -0700

    Inheritance and Method Overriding Completed

---
+++ InheritanceAndMethodOverriding/InheritanceAndMethodOverriding.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace InheritanceAndOverriding
+{
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal genericAnimal = new Animal();
+            Animal dog = new Dog();
+            Animal cat = new Cat();
+
+            genericAnimal.MakeSound();
+            dog.MakeSound();
+            cat.MakeSound();
+        }
+    }
+}
